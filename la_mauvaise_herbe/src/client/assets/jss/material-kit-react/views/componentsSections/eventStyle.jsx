ed62382e5df56363f9865820e827670a088039dd
camille <camillechassaing.jdb@gmail.com>
2019-09-02 11:46:19 +0200

    modification du chemin des pages ,création dossier client

---
+++ la_mauvaise_herbe/src/client/assets/jss/material-kit-react/views/componentsSections/eventStyle.jsx
@@ -0,0 +1,47 @@
+import { container, title } from "../../..//material-kit-react.jsx";
+
+const eventStyle = {
+sections: {
+    padding: "70px 0"
+  },
+  container,
+  marginAuto: {
+    marginLeft: "auto !important",
+    marginRight: "auto !important"
+  },
+  title: {
+    ...title,
+    marginTop: "30px",
+    minHeight: "32px",
+    textDecoration: "none"
+  },
+  h3: {
+    fontWeight: "500",
+    textAlign: "center",
+    fontSize: "2.5vw",
+  },
+  h4: {
+    fontWeight: "300",
+    textAlign: "center",
+    fontSize: "1.8vw",
+  },
+  p: {
+    fontWeight: "200",
+    textAlign: "center",
+    fontSize: "1.2vw",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    display: "block",
+  },
+  img: {
+    width: "100%",
+    height: "70vh",
+    "@media (max-width: 425px)": {
+        height: "40vh",
+      },
+  }
+};
+
+export default eventStyle;
---
+++ la_mauvaise_herbe/src/client/views/LesActivites/SectionLactivite.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+
+import withStyles from "@material-ui/core/styles/withStyles";
+
+
+import lactiviteStyle from "../../assets/jss/material-kit-react/views/componentsSections/lactiviteStyle.jsx";
+import Photo from "../../assets/img/cueillettes.jpg";
+import Reservation from "./SectionReservationActivite.jsx";
+
+
+class SectionLactivite extends React.Component {
+    oneActivity= () => {
+        const { classes } = this.props;
+        var anim = []
+        for (let i = 0; i < 3; i++) {
+            anim.push(
+                <div className={classes.container}>
+                <div className={classes.bloc}>
+                    <h3 className={classes.title}>Plantes médicinales sauvages</h3>
+                    <div className={classes.ligne}>
+                        <img className={classes.image} src={Photo} alt="Cueillette de plantes médicinales sauvages" />
+                        <div className={classes.colonne}>
+                            <h4 className={classes.titre}>Activites:</h4>
+                            <p><strong>Cueillette de plantes médicinales sauvages.</strong>
+                            Partons à la découverte de quelques plantes médicinales et apprenons
+                             à les cueillir. Je vous conterai leurs histoires, passées ou présentes 
+                             et leurs usages, reconnus ou dépassés. 
+                            </p>
+                            <div className={classes.ligne2}>
+                                <div className={classes.left}>
+                                    <div className={classes.tarif}>
+                                        <h5 className={classes.bold}>Tarif:</h5>
+                                        <p className={classes.valeur}>Variable </p>
+                                    </div>
+                                    <div className={classes.duree}>
+                                        <h5 className={classes.bold}>Durée:</h5>
+                                        <p className={classes.valeur}>3 h 00 </p>
+                                    </div>                                   
+                                </div>
+                                <div className={classes.right}>
+                                    <Reservation/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            )
+        }
+        return anim
+    }
+    
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.container}>                
+                    {this.oneActivity()}
+            </div>
+        );
+    }
+}
+
+SectionLactivite.propTypes = {
+    classes: PropTypes.object
+  };
+  
+  export default withStyles(lactiviteStyle)(SectionLactivite);
